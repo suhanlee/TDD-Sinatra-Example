d4bad5cc4bd547d2f3602c1190f52b8cf567ed3a
suhanlee <suhan.lee.k@gmail.com>
2016-04-26 00:59:46 +0900

    add function to show login-form when get to /login route.

--- login-01/app.rb
+++ login-01/app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/login' do
-
+    erb :login_form
 end
--- login-01/spec/login_spec.rb
+++ login-01/spec/login_spec.rb
@@ -2,7 +2,11 @@ require File.expand_path '../spec_helper.rb', __FILE__
 
 describe 'Login Path(/login)' do
   it 'show login input-form' do
+    user_name_tag = '<input type="text" name="user_name"'
+    password_tag = '<input type="password" name="password"'
     get('/login')
     expect(last_response.ok?).to eq(true)
+    expect(last_response.body).to include(user_name_tag)
+    expect(last_response.body).to include(password_tag)
   end
 end

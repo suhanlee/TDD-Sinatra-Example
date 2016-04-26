f6e2ecfbe239c9c9e74076f1ff7d463d1ea5a931
suhanlee <suhan.lee.k@gmail.com>
2016-04-26 10:36:27 +0900

    add Simple Login function

--- login-01/app.rb
+++ login-01/app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
 
+enable :sessions
+
+get '/' do
+  'Login Error' if get_error_session!
+end
+
 get '/login' do
     erb :login_form
+end
+
+post '/login' do
+  user_name = params[:user_name]
+  password = params[:password]
+  if user_name == 'testid' && password == 'password'
+    redirect to('/')
+  else
+    session[:error] = true
+  end
+end
+
+def get_error_session!
+  if session[:error]
+    error = session[:error]
+    session[:error] = nil
+    return error
+  end
 end
--- login-01/spec/login_spec.rb
+++ login-01/spec/login_spec.rb
@@ -4,9 +4,45 @@ describe 'Login Path(/login)' do
   it 'show login input-form' do
     user_name_tag = '<input type="text" name="user_name"'
     password_tag = '<input type="password" name="password"'
-    get('/login')
+    get '/login'
     expect(last_response.ok?).to eq(true)
     expect(last_response.body).to include(user_name_tag)
     expect(last_response.body).to include(password_tag)
   end
+
+  it 'after login, redirect to root or not' do
+    # right login
+    user_name = 'testid'
+    password = 'password'
+    post '/login', :user_name => user_name, :password => password
+
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_request.url).to eq('http://example.org/')
+
+    # wrong login
+    user_name = 'abcd'
+    password = '123'
+    post '/login', :user_name => user_name, :password => password
+    expect(last_response.ok?).to eq(true)
+  end
+
+  it 'try login with wrong user_name and display error message' do
+    # right login
+    user_name = 'testid'
+    password = 'password'
+    post '/login', :user_name => user_name, :password => password
+    get '/'
+
+    expect(last_response.body).not_to include('Login Error')
+
+    # wrong login
+    user_name = 'abcd'
+    password = '123'
+    post '/login', :user_name => user_name, :password => password
+    get '/'
+
+    expect(last_response.body).to include('Login Error')
+  end
+
 end
